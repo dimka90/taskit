3c79926ccf40d57c63d0869293f2de5a87559b77
dimka90 <dimkayilrit@gmail.com>
2026-02-04 08:29:52 +0100

    update:update

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/dimka90/taskit/internal"
 )
 
 func main() {
 
-	task := internal.SimplifyTask{"dimka", "Cooking", false}
-	fmt.Println(task.MarkComplete())
-	fmt.Println(task)
+	task := internal.NewSimplifyTask("Cooking")
+	task3 := internal.NewSimplifyTask("Washing Plate")
+
+	task2 := internal.NewPriorityTask(" Run for president", 1)
+	taskManager := internal.NewTaskManager()
+	taskManager.AddTask(task)
+	taskManager.AddTask(task2)
+	taskManager.AddTask(task3)
+	taskManager.SaveToFile("tasks.json")
+	taskManager.ListTask()
 }
